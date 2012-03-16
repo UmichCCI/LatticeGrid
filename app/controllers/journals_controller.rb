f758e20187727273453fd11869506859a96266c4
Rocky Fischer <adorack@eventide.local>
2012-03-16 12:45:01 -0400

    Updating seems to work.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -3,18 +3,18 @@ class JournalsController < ApplicationController
 	def set_high_impact_journals
 		# Request method is restricted to post.
 
-		if request.method == :post
-
-			render :text => Journal.find(:all, :conditions => {:id => params[:journal_ids]}).map(&:journal_abbreviation).join("\n"), :content_type => 'text/plain'
-			return
-
-			Journal.transaction do
-				Journal.update_all :include_as_high_impact => false
-				Journal.update_all :include_as_high_impact => true, :id => params[:journal_id]
-			end
-		else
-			# The CCSG page includes this page and 
+		Journal.transaction do
+			Journal.update_all(:include_as_high_impact => false)
+			Journal.update_all({:include_as_high_impact => true}, {:id => params[:journal_ids]})
 		end
+
+		# Technically, it's possible that someone could go to /journals/high_impact_journals.
+		# It will render correctly, and they could set high-impact journals from there.
+		# But the real interface is through CCSG.
+		# So we'll redirect back there and worry about this if it actually becomes a problem.
+		flash[:ccsg_notice] = "High-impact journals successfully updated."
+		redirect_to '/ccsg'
+
 	end
 
 	def high_impact_journals
