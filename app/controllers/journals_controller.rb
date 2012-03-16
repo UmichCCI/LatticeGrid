66b9246a852f63a0f4281f5ed46d3cd8ca92bb63
Rocky Fischer <adorack@eventide.local>
2012-03-16 11:53:00 -0400

    Further work on the interface: Added a jQuery library for dual list boxes.
    Things yet to do:
    
    - The library doesn't handle filtering the result list properly: fix its onsubmit handler or take params from the storage as well.
    - Make sure the Ajax call to load the high-impact list works.
    - Test that it actually works.
    - Get "restore defaults" working.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -1,16 +1,34 @@
 class JournalsController < ApplicationController
 
-	def set_high_impact
-		# Request method is restricted to either get or post.
+	def set_high_impact_journals
+		# Request method is restricted to post.
 
 		if request.method == :post
+
+			render :text => Journal.find(:all, :conditions => {:id => params[:journal_ids]}).map(&:journal_abbreviation).join("\n"), :content_type => 'text/plain'
+			return
+
 			Journal.transaction do
 				Journal.update_all :include_as_high_impact => false
 				Journal.update_all :include_as_high_impact => true, :id => params[:journal_id]
 			end
 		else
-			
+			# The CCSG page includes this page and 
 		end
+	end
+
+	def high_impact_journals
+		@journals = Journal.find(:all, :order => "journal_abbreviation")
+
+		if not request.xhr?
+			@javascripts_add = ['jquery.min', 'jquery.dualListBox-1.3.min']
+			@configure_boxes = true
+			layout = true
+		else
+			layout = false
+		end
+
+		render :layout => layout
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,8 @@ ActionController::Routing::Routes.draw do |map|
   map.org_wheel_data 'graphviz/:id/org_wheel_data.js', :action =>'org_wheel_data', :controller => 'graphviz', :conditions => {:method=>[:get,:post]}
   map.connect 'orgs/abstracts_during_period/:id', :controller => 'orgs', :action => 'abstracts_during_period'
   map.connect 'orgs/highimpactabstracts_during_period/:id', :controller => 'orgs', :action => 'highimpactabstracts_during_period'
-  map.set_high_impact 'orgs/set_high_impact', :controller => 'orgs', :action => 'set_high_impact', :conditions => { :method => :post }
+  map.set_high_impact_journals 'journals/set_high_impact_journals', :controller => 'journals', :action => 'set_high_impact_journals', :conditions => { :method => :post }
+  map.high_impact_journals 'journals/high_impact_journals', :controller => 'journals', :action => 'high_impact_journals', :conditions => { :method => :get }
   map.connect 'ccsg', :controller => 'profiles', :action => 'ccsg', :conditions => { :method => :get }
   map.connect 'admin', :controller => 'profiles', :action => 'ccsg', :conditions => { :method => :get }
   map.tag_cloud 'tag_cloud', :controller => 'abstracts', :action => 'tag_cloud', :conditions => { :method => :get }
