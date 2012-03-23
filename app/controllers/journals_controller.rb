45c8cecd9d00561f683c5ea6c5f0b348fd5d6eb5
Rocky Fischer <adorack@eventide.local>
2012-03-23 15:47:15 -0400

    Added the backend for restoring defaults.

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -3,9 +3,20 @@ class JournalsController < ApplicationController
 	def set_high_impact_journals
 		# Request method is restricted to post.
 
-		Journal.transaction do
-			Journal.update_all(:include_as_high_impact => false)
-			Journal.update_all({:include_as_high_impact => true}, {:journal_abbreviation => params[:journal_abbrs]})
+		if params[:commit] == "Set High Impact Journals"
+			Journal.transaction do
+				Journal.update_all(:include_as_high_impact => false)
+				Journal.update_all({:include_as_high_impact => true}, {:journal_abbreviation => params[:journal_abbrs]})
+			end
+		elsif params[:commit] == "Restore Defaults"
+			Journal.transaction do
+				Journal.update_all(:include_as_high_impact => false)
+				Journal.update_all({:include_as_high_impact => true}, {:issn => LatticeGridHelper.high_impact_issns})
+			end
+		else
+			# Being strict about this because checking submit button values isn't too common.
+			render :text => "Commit value not supported: #{params[:commit]}", :status => 400
+			return
 		end
 
 		# Technically, it's possible that someone could go to /journals/high_impact_journals.
@@ -14,12 +25,13 @@ class JournalsController < ApplicationController
 		# So we'll redirect back there and worry about this if it actually becomes a problem.
 		flash[:ccsg_notice] = "High-impact journals successfully updated."
 		redirect_to '/ccsg'
-
 	end
 
 	def high_impact_journals
 		@journals = Journal.find(:all, :order => "journal_abbreviation")
 
+		# TODO: Maybe not even support this request?  It would simplify the code.
+		# Leaving it in for now because who knows how the CCSG page will change.
 		if not request.xhr?
 			@javascripts_add = ['jquery.min', 'jquery.dualListBox-1.3']
 			@configure_boxes = true
