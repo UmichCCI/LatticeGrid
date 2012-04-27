5c30e28947295f3afb9ba859f6418c550395a082
Rocky Fischer <adorack@eventide.local>
2012-04-27 15:20:33 -0400

    Split build into two parts.

--- script/UMich/cron/monthly.sh
+++ script/UMich/cron/monthly.sh
@@ -11,7 +11,8 @@
 
 echo "Beginning monthly build on $(date)"
 
-bundle exec rake RAILS_ENV=production monthlyBuild 2>&1 >> log/monthly_rake.log
+bundle exec rake RAILS_ENV=production monthlyBuild1 2>&1 >> log/monthly_rake.log
+bundle exec rake RAILS_ENV=production monthlyBuild2 2>&1 >> log/monthly_rake.log
 bundle exec rake RAILS_ENV=production db:vacuum
 
 echo "Finished building mesh on $(date)"
