7a1135dbba9519f1f19a49c1eb3e742f7c875898
Rocky Fischer <adorack@eventide.local>
2012-05-01 10:34:16 -0400

    Merged the "get_members_from_db" and "parse_members_from_db" scripts.
    There's no reason to have them separate.

--- script/UMich/get_members_from_db.rb
+++ script/UMich/get_members_from_db.rb
@@ -1,6 +1,14 @@
+if ARGV.empty?
+	$stderr.puts "Usage: #{$0} output_file"
+	$stderr.puts
+	$stderr.puts "This file dumps formatted data from the database view, identifing the primary department and adjusting the member_type values."
+	exit
+end
+
 require 'rubygems'
 require "bundler/setup"
 
+require 'fastercsv'
 require 'oci8'
 
 require './db/cto_oracle_settings'
@@ -9,15 +17,39 @@ include Settings  # Defines the USER, PASSWORD, DSN, and TABLE_* constants.
 conn = OCI8.new(USER, PASSWORD, DSN)
 cur = conn.exec("SELECT * FROM %s.%s" % [TABLE_OWNER, MEMBER_TABLE_NAME])
 
-hdict = {'LAST_NAME' => 'last_name', 'FIRST_NAME' => 'first_name', 'MIDDLE_NAME' => 'middle_name', 'DEGREE' => 'degrees', 'TITLE_NAME' => 'rank', 'DEPARTMENT_NAME' => 'department', 'PROGRAM_CODE' => 'program_num', 'PROGRAM_NAME' => 'program', 'PUBMED_SEARCH_NAME' => 'pubmed_search_name', 'PUBMED_LIMIT_TO_INSTITUTION' => 'pubmed_limit_to_institution', 'EMAIL_ADDRESS' => 'email', 'UNIQNAME' => 'username', 'EMPID' => 'employee_id', 'ASSOCIATION_NAME' => 'assoc_name', 'PRIMARY_PROGRAM_CODE' => 'home_department_id'}
+hmap = {'LAST_NAME' => 'last_name', 'FIRST_NAME' => 'first_name', 'MIDDLE_NAME' => 'middle_name', 'DEGREE' => 'degrees', 'TITLE_NAME' => 'rank', 'DEPARTMENT_NAME' => 'department', 'PROGRAM_CODE' => 'program_num', 'PROGRAM_NAME' => 'program', 'PUBMED_SEARCH_NAME' => 'pubmed_search_name', 'PUBMED_LIMIT_TO_INSTITUTION' => 'pubmed_limit_to_institution', 'EMAIL_ADDRESS' => 'email', 'UNIQNAME' => 'username', 'EMPID' => 'employee_id', 'ASSOCIATION_NAME' => 'assoc_name', 'PRIMARY_PROGRAM_CODE' => 'home_department_id'}
 
-headers = cur.get_col_names
-header_out = headers.select{|h| hdict.has_key? h }
+headers = cur.get_col_names  # SOMETHING_PK, LAST_NAME, FIRST_NAME, ...
+header_out = headers.select{|h| hmap.has_key? h }  # LAST_NAME, FIRST_NAME, ...
+human_headers = header_out.map{|h| hmap[h] }  #  last_name, first_name, ...
+file_headers = ['member_type'] + human_headers - ['assoc_name']
 
-puts header_out.map{|h| hdict[h] }.join("\t")
+FasterCSV.open(ARGV[0], "w", :headers => file_headers, :col_sep => "\t", :write_headers => true) do |csv|
+	while dbrow = cur.fetch_hash()
 
-while row = cur.fetch_hash()
-	puts row.values_at(*header_out).join("\t")
+		row = Hash[*human_headers.zip(dbrow.values_at(*header_out)).flatten]
+
+		if row['program_num'] == row['home_department_id']
+			apt_type = 'Primary'
+		else
+			apt_type = 'Secondary'
+		end
+
+		if row['assoc_name'] == 'Member'
+			apt_name = 'AssociateMember'
+		elsif row['assoc_name'] = 'Core Member'
+			apt_name = 'Member'
+		else
+			raise "Invalid association name: #{row['assoc_name']}."
+		end
+
+		row['member_type'] = apt_type + apt_name
+
+		# Not strictly necessary to delete this, but it's unnecessary.
+		row.delete('assoc_name')
+
+		csv << row
+	end
 end
 
 cur.close()
