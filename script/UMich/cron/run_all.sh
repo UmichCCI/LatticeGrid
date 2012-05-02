5c8ff2ac07bf9181d86a56642b14e061dacc173f
Rocky Fischer <adorack@latticegrid-dev.msis.pd>
2012-05-02 12:21:33 -0400

    Changes to the cron so that everything runs with minimal permissions.

--- script/UMich/cron/run_all.sh
+++ script/UMich/cron/run_all.sh
@@ -2,7 +2,10 @@
 
 # This script is run daily by cron.  It takes care of nightlyBuild, runs monthlyBuild if it needs to, and then does caching.  This way, all the scripts are guaranteed to finish before the next one begins, and caching only happens once when running monthlyBuild.
 
-# Sample crontab entry:
+# This script shouldn't be run by root--it's owned by a normal user.  However, the cache script should only run after everything here finishes.  This makes the crontab entry complicated:
+# 10 1 * * * root (cd /var/www/apps/umich_latticegrid/ && su --shell=/bin/bash --session-command='env PATH=/opt/ruby-enterprise-1.8.7-2012.02/bin/:$PATH script/UMich/cron/run_all.sh' adorack 2>&1 >> log/cron.log && su --shell=/bin/bash --session-command='env PATH=/opt/ruby-enterprise-1.8.7-2012.02/bin/:$PATH script/UMich/cron/cache.sh' nobody 2>&1 > log/cache.log)
+
+# In contrast, say the cache directories didn't need to be owned by "nobody".  Then, can just run this script directly as a normal user:
 # 10 1 * * * adorack (PATH=/opt/ruby-enterprise-1.8.7-2012.02/bin:$PATH; cd /var/www/apps/umich_latticegrid/ && script/UMich/cron/run_all.sh 2>&1 >> log/cron.log)
 
 # The monthly build task will run on this day (DD format):
@@ -28,9 +31,5 @@ if [[ `date "+%d"` -eq $mday ]]; then
 	script/UMich/cron/monthly.sh 2>&1 > log/monthly_rake.log
 fi
 
-# The cache script does output the time it starts, but that goes into cache.log not cron.log.
-echo "Starting to build cache on $(date)"
-# cache.sh needs to run as nobody so that it can write to the appropriate directories in "public."  Output is written to cache.log as the current user, however.
-sudo -u nobody env PATH=/opt/ruby-enterprise-1.8.7-2012.02/bin/:$PATH script/UMich/cron/cache.sh 2>&1 > log/cache.log
-
-echo "Finished cron jobs on $(date)"
+# Cron will run the cache script next as a different user.
+echo "Main tasks finished on $(date).  Cache build is next."
